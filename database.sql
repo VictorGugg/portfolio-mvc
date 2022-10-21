5e563a86a22fed5da02b9caa33cd90f96c4e04f5
VictorGugg <victor.guggenbuhl@gmail.com>
2022-10-21 15:05:48 +0200

    Trimmed a mono-space in front of the many instances of the 'article' word.

--- database.sql
+++ database.sql
@@ -36,9 +36,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `portfolio`.` article`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `portfolio`.` article` ;
+DROP TABLE IF EXISTS `portfolio`.`article` ;
 
-CREATE TABLE IF NOT EXISTS `portfolio`.` article` (
+CREATE TABLE IF NOT EXISTS `portfolio`.`article` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
   `text` TEXT(5000) NULL,
@@ -80,22 +80,22 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `portfolio`.` article_has_image`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `portfolio`.` article_has_image` ;
+DROP TABLE IF EXISTS `portfolio`.`article_has_image` ;
 
-CREATE TABLE IF NOT EXISTS `portfolio`.` article_has_image` (
-  ` article_id` INT UNSIGNED NOT NULL,
-  ` article_user_id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `portfolio`.`article_has_image` (
+  `article_id` INT UNSIGNED NOT NULL,
+  `article_user_id` INT UNSIGNED NOT NULL,
   `image_id` INT UNSIGNED NOT NULL,
   `image_user_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (` article_id`, ` article_user_id`, `image_id`, `image_user_id`),
-  INDEX `fk_ article_has_image_image1_idx` (`image_id` ASC, `image_user_id` ASC),
-  INDEX `fk_ article_has_image_ article1_idx` (` article_id` ASC, ` article_user_id` ASC),
-  CONSTRAINT `fk_ article_has_image_ article1`
-    FOREIGN KEY (` article_id` , ` article_user_id`)
-    REFERENCES `portfolio`.` article` (`id` , `user_id`)
+  PRIMARY KEY (`article_id`, `article_user_id`, `image_id`, `image_user_id`),
+  INDEX `fk_article_has_image_image1_idx` (`image_id` ASC, `image_user_id` ASC),
+  INDEX `fk_article_has_image_ article1_idx` (`article_id` ASC, `article_user_id` ASC),
+  CONSTRAINT `fk_article_has_image_article1`
+    FOREIGN KEY (`article_id` , `article_user_id`)
+    REFERENCES `portfolio`.`article` (`id` , `user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ article_has_image_image1`
+  CONSTRAINT `fk_article_has_image_image1`
     FOREIGN KEY (`image_id` , `image_user_id`)
     REFERENCES `portfolio`.`image` (`id` , `user_id`)
     ON DELETE NO ACTION
