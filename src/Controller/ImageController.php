1daf52e832a5a38216b43f009cc875076da4fefe
VictorGugg <victor.guggenbuhl@gmail.com>
2022-10-27 11:26:02 +0200

    Commit use for a grouped debugging session : validation doesn't work, nor do the edition. Checkout => ImageController.php

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -4,8 +4,86 @@ namespace App\Controller;
 
 use App\Model\ImageManager;
 
-class ImageController extends AbstractController
+final class ImageController extends AbstractController
 {
+    /** Creating empty variables matching the form input
+     * TODO add the url input
+     */
+
+    // public $title = '';
+    // public $description = '';
+    // public $url = '';
+
+    /**
+     * Creating an errors array, to store potential errors
+     * and be able to report them to the user.
+     */
+
+    // public $errors = [];
+
+
+    /**
+     * Sanitization + error rendering function
+     */
+
+    // public function validation()
+    // {
+    //     if (
+            /* Checking for an input, other than only spaces, of 200 or less characters */
+        //     !isset($_POST['title'])
+        //     || trim($_POST['title']) === ''
+        //     || strlen($_POST['title']) > 200
+        // ) {
+        //     $errors[] = "title=1";
+        // } else {
+        //     return (string)$title = $this->sanitizeInput($_POST['title']);
+        // }
+
+        // if (
+            /* Checking for an input, other than only spaces, of 1.000 or less characters */
+        //     !isset($_POST['description'])
+        //     || trim($_POST['description']) === ''
+        //     || strlen($_POST['description']) > 1000
+        // ) {
+        //     $errors[] = "description=1";
+        // } else {
+        //     return (string)$description = $this->sanitizeInput($_POST['description']);
+        // }
+
+        // if (
+            /* Checking for an input, other than only spaces, of 2048 or less characters */
+        //     !isset($_POST['url'])
+        //     || trim($_POST['url']) === ''
+        //     || strlen($_POST['url']) > 2048
+        // ) {
+        //     $errors[] = "url=1";
+        // } else {
+        //     return (string)$url = $this->sanitizeInput($_POST['url']);
+        // }
+
+        /* Checking if there is errors and displaying the appropriate message(s) to the user */
+
+    //     if (!empty($errors)) {
+    //         $errorsJoined = join('&', $errors);
+    //         header("Location: /add.php?errors=1&" . $errorsJoined);
+    //     }
+
+    //     if (isset($_GET['errors'])) {
+    //         if (isset($_GET['title'])) {
+    //             echo "Please enter a valid title (200 characters max).<br>";
+    //         }
+
+    //         if (isset($_GET['description'])) {
+    //             echo "Please enter a valid lastname (1.000 characters max).<br>";
+    //         }
+
+    //         if (isset($_GET['url'])) {
+    //             echo "Please enter a valid url (2.048 characters max).<br>";
+    //         }
+    //     }
+    // }
+
+
     /**
      * List images
      * TODO : Display a miniature of the image
@@ -38,13 +116,13 @@ class ImageController extends AbstractController
         $imageManager = new ImageManager();
         $image = $imageManager->selectOneById($id);
 
+        // Possibilité made in Yacine : factoriser en différentes fonctions
+        // $data = [];
+        // $data['title'] = resultOfMyValidation();
+        // $imageInstance->setProperties($data);
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $image = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, update and redirection
+            // $this->validation();
             $imageManager->update($image);
 
             header('Location: /images/show?id=' . $id);
@@ -66,8 +144,7 @@ class ImageController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // clean $_POST data
             $image = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
+            // $this->validation();
 
             // if validation is ok, insert and redirection
             $imageManager = new ImageManager();
--- src/Model/ImageManager.php
+++ src/Model/ImageManager.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use PDO;
 
-class ImageManager extends AbstractManager
+final class ImageManager extends AbstractManager
 {
     public const TABLE = 'image';
 
@@ -15,9 +15,10 @@ class ImageManager extends AbstractManager
     public function insert(array $image): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
-        " (`title`, `description`) VALUES (:title, :description)");
-        $statement->bindValue('title', $image['title'], PDO::PARAM_STR);
-        $statement->bindValue('description', $image['description'], PDO::PARAM_STR);
+        " (`title`, `description`, `url`) VALUES (:title, :description, :url)");
+        $statement->bindValue(':title', $image['title'], PDO::PARAM_STR);
+        $statement->bindValue(':description', $image['description'], PDO::PARAM_STR);
+        $statement->bindValue(':url', $image['url'], PDO::PARAM_STR);
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
@@ -30,10 +31,11 @@ class ImageManager extends AbstractManager
     public function update(array $image): bool
     {
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
-        " SET `title` = :title, `description` = :description WHERE id=:id");
+        " SET `title` = :title, `description` = :description, `url` = :url WHERE id=:id");
         $statement->bindValue('id', $image['id'], PDO::PARAM_INT);
         $statement->bindValue('title', $image['title'], PDO::PARAM_STR);
         $statement->bindValue('description', $image['description'], PDO::PARAM_STR);
+        $statement->bindValue('url', $image['url'], PDO::PARAM_STR);
 
         return $statement->execute();
     }
